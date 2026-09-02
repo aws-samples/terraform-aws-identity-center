7069850dff66f868a48d4d54ff07c85a04899397
Jake <94998878+jakebark@users.noreply.github.com>
2026-09-02 17:13:33 +0100

    fix: update yaml to be optional (#22)
    update yaml to be optional (all lines not required)

--- locals.tf
+++ locals.tf
@@ -2,7 +2,24 @@
 // SPDX-License-Identifier: MIT-0
 
 locals {
-  permission_sets = can(regex("\\.tpl$", var.permission_sets)) ? yamldecode(templatefile(var.permission_sets, var.template_variables)) : yamldecode(file(var.permission_sets))
+  permission_set_defaults = {
+    description               = null
+    tags                      = null
+    relay_state               = null
+    session_duration          = null
+    inline_policy             = null
+    aws_managed_policies      = null
+    customer_managed_policies = null
+    permissions_boundary      = null
+  }
+
+  permission_sets_raw = can(regex("\\.tpl$", var.permission_sets)) ? yamldecode(templatefile(var.permission_sets, var.template_variables)) : yamldecode(file(var.permission_sets))
+
+  // merge raw and defaults. enables users to create yaml files with just the minimum key value pairs
+  permission_sets = {
+    for name, permission_set in local.permission_sets_raw :
+    name => merge(local.permission_set_defaults, permission_set)
+  }
 
   account_assignments = can(regex("\\.tpl$", var.account_assignments)) ? yamldecode(templatefile(var.account_assignments, var.template_variables)) : yamldecode(file(var.account_assignments))
 }
